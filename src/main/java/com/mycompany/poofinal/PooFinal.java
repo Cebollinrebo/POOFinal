223767b3ff1c9e84abe7313ef35387df2f52e9b6
empanada25 <empanada25@empanada25-ROG-Flow-X13-GV301RC-GV301RC>
2024-11-30 14:02:54 -0600

    Inicializacion

---
+++ src/main/java/com/mycompany/poofinal/PooFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poofinal;
+
+/**
+ *
+ * @author empanada25
+ */
+public class PooFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
